9b64c0e5c658f9a4e1676a5212126f010a0f6565
Ekaterina Aedonitskaia <kate.aedon@gmail.com>
2021-06-01 10:03:47 +0200

    add app.js file, add user schema

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app
+  .use(cookieParser())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening to PORT ${PORT}`);
+});
